0df9bc595b277efcaee773d3de9734a0a0504aa6
Alexey-Kapitonov <dekaliostro@gmail.com>
2020-12-13 17:29:19 +0200

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -4,12 +4,27 @@ console.log("Task-2");
 console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 /*
-
+Напиши функцию countProps(obj), 
+считающую кол-во свойств в объекте. 
+Функция возвращает число - количество свойств.
 */
 
 //Task START
 
+const countProps = function(obj) {
+  // твой код
+    return Object.keys(obj).length;  
+   
+}; 
 
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
 
 //Task END
 
