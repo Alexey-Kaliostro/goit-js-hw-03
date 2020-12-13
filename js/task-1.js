9c1570b207bdb3d62cacdd7f1d97e44892d526eb
Alexey-Kapitonov <dekaliostro@gmail.com>
2020-12-13 17:27:56 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -5,12 +5,39 @@ console.log(" ");
 //=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-
 
 /*
+Напиши скрипт, который, для объекта user, последовательно:
 
+добавляет поле mood со значением 'happy'
+заменяет значение hobby на 'skydiving'
+заменяет значение premium на false
+выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of
 */
 
 // Task START
 
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
 
+user.mood = "happy";
+user["hobby"] = "skydiving"
+user.premium = false;
+
+let objectKeys = "";
+let objectKeysValue = "";
+let list= "";
+
+keys = Object.keys(user);
+
+for (const keys in user) {
+    
+    objectKeysValue = user[keys];
+    list = `${keys}: ${objectKeysValue}`;
+   console.log(list);
+}  
 
 //Task END
 
